91db593cbd5d80085774ded000999969233514b4
felipe.borges <felipe.borges@meiuca.design>
2021-07-25 03:54:04 -0300

    product consuming jota-ds

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
 import logo from './logo.svg';
+import { Button, Menu, Modal, Typography } from '@jota-ds/lib-react';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [modalIsOpen, setOpenModal] = useState(false);
+
+  const openModal = () => {
+    setOpenModal(true);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Button label="Jota DS" type="primary" onColor={true} handleClick={openModal}/>
+        <Modal isOpen={modalIsOpen} firstActionLabel="Ok" secondActionLabel="Cancel" isDoubleAction={true} handleConfirm={() => console.log("confirmou")} handleCancel={() => console.log("cancelou")} handleClose={() => setOpenModal(false)}>
+          <Typography component="heading" size="large">Hello, World!</Typography>
+          <br></br>
+          <Typography component="paragraph" size="small">Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel dolorum cumque perspiciatis nobis accusantium provident recusandae libero voluptatum maxime placeat.</Typography>
+          <br></br>
+          <Typography component="paragraph" size="small">Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel dolorum cumque perspiciatis nobis accusantium provident recusandae libero voluptatum maxime placeat.</Typography>
+        </Modal>
       </header>
     </div>
   );
